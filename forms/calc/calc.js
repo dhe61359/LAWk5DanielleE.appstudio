02d962ec516be7f03b1079dce6f63c150b1728d9
Danielle Eng <dhe61359@creighton.edu>
2021-03-03 15:57:59 -0600

    third

---
+++ forms/calc/calc.js
@@ -0,0 +1,12 @@
+num1 = Number(prompt("Give me a number."))
+num2 = Number(prompt("Give me another number."))
+
+summedNumber = num1 + num2
+
+function Calculate(num1, num2) {
+    console.log(`The sum of ${num1} and ${num2} is ${summedNumber}.`)
+    }
+    
+Calculate(num1, num2)
+
+
